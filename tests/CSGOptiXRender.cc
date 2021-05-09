52c104bb7f5e1b7b8b71bac5ce4d5ab13bc8b68c
Simon C Blyth <simoncblyth@gmail.com>
2021-05-09 01:28:32 +0100

    use getCenterExtent for midx/mord/iidx

--- tests/CSGOptiXRender.cc
+++ tests/CSGOptiXRender.cc
@@ -12,47 +12,22 @@ int main(int argc, char** argv)
     Opticks ok(argc, argv); 
     ok.configure(); 
 
-    int meshIdx = SSys::getenvint("MIDX", 130);   // 130 is world box for default CFBASE
-    int ordinal = SSys::getenvint("ORDINAL", 0 ); 
+    int midx = SSys::getenvint("MIDX", 130);   // 130 is world box for default CFBASE
+    int mord = SSys::getenvint("MORD",   0); 
+    int iidx = SSys::getenvint("IIDX",   0);
 
     const char* outdir = SSys::getenvvar("OUTDIR", "/tmp" ); 
     const char* top    = SSys::getenvvar("TOP", "i0" ); 
     const char* cfbase = SSys::getenvvar("CFBASE", "$TMP/CSG_GGeo" );
-
 
     CSGFoundry* foundry = CSGFoundry::Load(cfbase, "CSGFoundry"); 
     foundry->upload(); 
     LOG(info) << "foundry " << foundry->desc() ; 
     foundry->summary(); 
 
-    // collect prim matching the MIDX and select the ORDINAL one
-    std::vector<CSGPrim> prim ; 
-    foundry->getMeshPrim(prim, meshIdx );  
-    bool in_range = ordinal < prim.size() ; 
-
-    if(!in_range)
-    {
-        LOG(info)  
-            << " meshIdx " << meshIdx
-            << " ordinal " << ordinal 
-            << " prim.size " << prim.size()
-            ;
-         return 1 ;   
-    }
-
-    const CSGPrim& pr = prim[ordinal] ;  
-    const float4 ce = pr.ce() ; 
-
-    LOG(info)
-        << " top " << top
-        << " ce " << ce 
-        << " ce.w " << ce.w 
-        << " NB : no instance transforming : so this only makes sense for global prim " 
-        ; 
-
-    // hmm for generality would also need to select an instance
-    // and look up the instance transform with which to transform the prim bbox 
-    // and get the instanced ce 
+    float4 ce = make_float4(0.f, 0.f, 0.f, 1000.f ); 
+    int rc = foundry->getCenterExtent(ce, midx, mord, iidx) ;
+    if(rc != 0) return 1 ; 
 
 
     CSGOptiX cx(&ok, foundry, outdir); 
