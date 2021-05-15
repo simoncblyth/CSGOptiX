ce3a1bed52de3979ab88a17da28175abd9ad460b
Simon C Blyth <simoncblyth@gmail.com>
2021-05-15 12:26:05 +0100

    view defaults for scaled_box3 that show the layers

--- cxr_demo.sh
+++ cxr_demo.sh
@@ -26,7 +26,8 @@ EOU
 
 #geometry=parade
 #geometry=layered_sphere
-geometry=sphere_containing_grid_of_spheres
+#geometry=sphere_containing_grid_of_spheres
+geometry=scaled_box3
 
 export GEOMETRY=${GEOMETRY:-$geometry}
 cfname=CSGDemoTest/$GEOMETRY            # picks the CSGFoundry geometry to load
@@ -35,14 +36,20 @@ if [ "$GEOMETRY" == "parade" ]; then    # geometry specific defaults, maybe over
    moi=0:0:4                            # what to look at 
    eye=-10,0,5,1                        # where to look from 
    tmin=0.1
+   cam=0         
+elif [ "$GEOMETRY" == "scaled_box3" ]; then
+   moi=-1
+   eye=-1,-1,0.5
+   tmin=1.1 
+   cam=1
 else
    moi=-1                               # -1 entire-IAS default as it applies to any geometry               
    eye=-1,0,1,1                         # 45 degree "tray" view 
    tmin=1
+   cam=0         
 fi 
 
-cam=0           # 0:perspective 1:ortho
-emm=t0          # default to no solid skips with demo geometry 
+emm=t0    # default to no solid skips with demo geometry 
 
 stamp=$(date +"%Y-%m-%d %H:%M")
 version=$(CSGOptiXVersion)
@@ -51,7 +58,7 @@ export CFNAME=${CFNAME:-$cfname}
 export EMM=${EMM:-$emm}
 export TMIN=${TMIN:-$tmin}
 export EYE=${EYE:-$eye}
-export CAM=${CAM:-$cam}
+export CAM=${CAM:-$cam}    # 0:perspective 1:ortho
 
 export NAMEPREFIX=cxr_demo_${GEOMETRY}_   # MOI is appended by tests/CSGOptiXRender.cc when --solid_label yields no solids
 export RELDIR=cxr_demo/cam_${CAM}
